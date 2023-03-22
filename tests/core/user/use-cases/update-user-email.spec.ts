be8af28c7d612b8fab79a5a38b256a0fa222fc04
João Pedro <joaopedromoraesnsilva@gmail.com>
2023-03-22 19:53:19 -0300

    test: add update user email use case unit test

---
+++ tests/core/user/use-cases/update-user-email.spec.ts
@@ -0,0 +1,93 @@
+import {
+  ExpenseRepositoryStub,
+  UnitOfWorkStub,
+  UserRepositoryStub,
+  UserVerificationCodeRepositoryStub,
+  testUserModel,
+} from '../../__mocks__';
+
+import { InvalidParamError, NotFoundError } from '@/core/errors';
+import { UpdateUserEmailUseCase } from '@/core/domain/users/use-cases';
+
+const makeSut = () => {
+  const userRepositoryStub = new UserRepositoryStub();
+  const userVerificationCodeRepositoryStub =
+    new UserVerificationCodeRepositoryStub();
+  const expenseRepository = new ExpenseRepositoryStub();
+  const unitOfWorkStub = new UnitOfWorkStub(
+    userRepositoryStub,
+    userVerificationCodeRepositoryStub,
+    expenseRepository,
+  );
+  const sut = new UpdateUserEmailUseCase(unitOfWorkStub);
+
+  return {
+    sut,
+    userRepositoryStub,
+  };
+};
+
+describe('Use case - UpdateUserEmailUseCase', () => {
+  test('Should not update user email, because user is not exists', async () => {
+    const id = '2';
+    const email = 'email@test2.com';
+    const code = 'code';
+    const { sut, userRepositoryStub } = makeSut();
+    jest
+      .spyOn(userRepositoryStub, 'getUserByIdWithVerificationCode')
+      .mockReturnValueOnce(null);
+
+    const result = await sut.execute({ id, email, code });
+
+    expect(result).toBeInstanceOf(NotFoundError);
+  });
+
+  test('Should not update user email, because code is invalid', async () => {
+    const id = '1';
+    const email = 'email@test2.com';
+    const invalidCode = 'invalid_code';
+    const { sut, userRepositoryStub } = makeSut();
+    jest
+      .spyOn(userRepositoryStub, 'getUserByIdWithVerificationCode')
+      .mockReturnValueOnce(
+        Promise.resolve({ ...testUserModel, userVerificationCode: null }),
+      );
+
+    const result = await sut.execute({ id, email, code: invalidCode });
+
+    expect(result).toBeInstanceOf(InvalidParamError);
+  });
+
+  test('Should not update user email, because code expiried', async () => {
+    const id = '1';
+    const email = 'email@test2.com';
+    const code = 'code';
+    const { sut, userRepositoryStub } = makeSut();
+    jest
+      .spyOn(userRepositoryStub, 'getUserByIdWithVerificationCode')
+      .mockReturnValueOnce(
+        Promise.resolve({
+          ...testUserModel,
+          userVerificationCode: {
+            ...testUserModel.userVerificationCode,
+            verificationCodeExpiryDate: 0,
+          },
+        }),
+      );
+
+    const result = await sut.execute({ id, email, code });
+
+    expect(result).toBeInstanceOf(InvalidParamError);
+  });
+
+  test('Should update user email', async () => {
+    const id = '1';
+    const email = 'email@test2.com';
+    const code = 'code';
+    const { sut } = makeSut();
+
+    const result = await sut.execute({ id, email, code });
+
+    expect(result).toBe(id);
+  });
+});
