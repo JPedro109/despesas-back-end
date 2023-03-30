b77664a2175375674dbcfee47d2462ceee6dab0a
João Pedro <joaopedromoraesnsilva@gmail.com>
2023-03-30 19:29:10 -0300

    feat: add user module

---
+++ src/core/domain/users/users.module.ts
@@ -0,0 +1,117 @@
+import { Module } from '@nestjs/common';
+import {
+  CryptographyModule,
+  DatabaseModule,
+  MailModule,
+  GenerationModule,
+  JsonWebTokenModule,
+} from '@/infra';
+import {
+  AbstractCreateUserUseCase,
+  AbstractDeleteUserUseCase,
+  AbstractRecoverUserPasswordUseCase,
+  AbstractSendUserEmailUpdateLinkUseCase,
+  AbstractSendUserPasswordRecoveryLinkUseCase,
+  AbstractUpdateUserEmailUseCase,
+  AbstractUpdateUserPasswordUseCase,
+  AbstractUserLoginUseCase,
+  AbstractUserVerifyEmailUseCase,
+} from './abstracts';
+import {
+  CreateUserUseCase,
+  DeleteUserUseCase,
+  RecoverUserPasswordUseCase,
+  SendUserEmailUpdateLinkUseCase,
+  SendUserPasswordRecoveryLinkUseCase,
+  UpdateUserEmailUseCase,
+  UpdateUserPasswordUseCase,
+  UserLoginUseCase,
+  UserVerifyEmailUseCase,
+} from './use-cases';
+
+@Module({
+  imports: [
+    CryptographyModule,
+    DatabaseModule,
+    MailModule,
+    GenerationModule,
+    JsonWebTokenModule,
+  ],
+  providers: [
+    {
+      provide: AbstractCreateUserUseCase,
+      useClass: CreateUserUseCase,
+    },
+    {
+      provide: AbstractDeleteUserUseCase,
+      useClass: DeleteUserUseCase,
+    },
+    {
+      provide: AbstractRecoverUserPasswordUseCase,
+      useClass: RecoverUserPasswordUseCase,
+    },
+    {
+      provide: AbstractSendUserEmailUpdateLinkUseCase,
+      useClass: SendUserEmailUpdateLinkUseCase,
+    },
+    {
+      provide: AbstractSendUserPasswordRecoveryLinkUseCase,
+      useClass: SendUserPasswordRecoveryLinkUseCase,
+    },
+    {
+      provide: AbstractUpdateUserEmailUseCase,
+      useClass: UpdateUserEmailUseCase,
+    },
+    {
+      provide: AbstractUpdateUserPasswordUseCase,
+      useClass: UpdateUserPasswordUseCase,
+    },
+    {
+      provide: AbstractUserLoginUseCase,
+      useClass: UserLoginUseCase,
+    },
+    {
+      provide: AbstractUserVerifyEmailUseCase,
+      useClass: UserVerifyEmailUseCase,
+    },
+  ],
+  exports: [
+    {
+      provide: AbstractCreateUserUseCase,
+      useClass: CreateUserUseCase,
+    },
+    {
+      provide: AbstractDeleteUserUseCase,
+      useClass: DeleteUserUseCase,
+    },
+    {
+      provide: AbstractRecoverUserPasswordUseCase,
+      useClass: RecoverUserPasswordUseCase,
+    },
+    {
+      provide: AbstractSendUserEmailUpdateLinkUseCase,
+      useClass: SendUserEmailUpdateLinkUseCase,
+    },
+    {
+      provide: AbstractSendUserPasswordRecoveryLinkUseCase,
+      useClass: SendUserPasswordRecoveryLinkUseCase,
+    },
+    {
+      provide: AbstractUpdateUserEmailUseCase,
+      useClass: UpdateUserEmailUseCase,
+    },
+    {
+      provide: AbstractUpdateUserPasswordUseCase,
+      useClass: UpdateUserPasswordUseCase,
+    },
+    {
+      provide: AbstractUserLoginUseCase,
+      useClass: UserLoginUseCase,
+    },
+    {
+      provide: AbstractUserVerifyEmailUseCase,
+      useClass: UserVerifyEmailUseCase,
+    },
+  ],
+})
+export class UsersModule {}
