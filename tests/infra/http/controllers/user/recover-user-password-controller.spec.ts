ebc170b405b92323f0ce4e13828a9d843e6b3500
João Pedro <joaopedromoraesnsilva@gmail.com>
2023-04-01 22:09:19 -0300

    test: add recover user password controller unit test

---
+++ tests/infra/http/controllers/user/recover-user-password-controller.spec.ts
@@ -0,0 +1,82 @@
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+import { RecoverUserPasswordController } from '@/infra/http/controllers';
+import { InvalidParamError, NotFoundError } from '@/core/errors';
+import { RecoverUserPasswordStub } from './stubs';
+
+const makeSut = () => {
+  const recoverUserPasswordStub = new RecoverUserPasswordStub();
+  const sut = new RecoverUserPasswordController(recoverUserPasswordStub);
+
+  return {
+    sut,
+    recoverUserPasswordStub,
+  };
+};
+
+const makeBody = (
+  email: string,
+  code: string,
+  password: string,
+  passwordConfirm: string,
+) => {
+  return {
+    email,
+    code,
+    password,
+    passwordConfirm,
+  };
+};
+
+describe('Infra (Controller) - RecoverUserPasswordController', () => {
+  test('Should not recover user password, because use case returned invalid param error', async () => {
+    const body = makeBody(
+      'email@test.com',
+      'code_invalid',
+      'password',
+      'password',
+    );
+    const { sut, recoverUserPasswordStub } = makeSut();
+    jest
+      .spyOn(recoverUserPasswordStub, 'execute')
+      .mockResolvedValueOnce(Promise.resolve(new InvalidParamError('error')));
+
+    await sut
+      .handle(
+        { email: body.email, code: body.code },
+        { password: body.password, passwordConfirm: body.passwordConfirm },
+      )
+      .catch((e) => expect(e).toBeInstanceOf(BadRequestException));
+  });
+
+  test('Should not recover user password, because use case returned not found error', async () => {
+    const body = makeBody('email.com', 'code_invalid', 'password', 'password');
+    const { sut, recoverUserPasswordStub } = makeSut();
+    jest
+      .spyOn(recoverUserPasswordStub, 'execute')
+      .mockResolvedValueOnce(Promise.resolve(new NotFoundError('error')));
+
+    await sut
+      .handle(
+        { email: body.email, code: body.code },
+        { password: body.password, passwordConfirm: body.passwordConfirm },
+      )
+      .catch((e) => expect(e).toBeInstanceOf(NotFoundException));
+  });
+
+  test('Should recover user password', async () => {
+    const body = makeBody(
+      'email@test.com',
+      'code',
+      'Password1234',
+      'Password1234',
+    );
+    const { sut } = makeSut();
+
+    const result = await sut.handle(
+      { email: body.email, code: body.code },
+      { password: body.password, passwordConfirm: body.passwordConfirm },
+    );
+
+    expect(result).toEqual(Object(body.email));
+  });
+});
