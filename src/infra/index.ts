9a6345f5adca006742fff109500400ecedd0e4d3
João Pedro <joaopedromoraesnsilva@gmail.com>
2023-04-08 19:11:28 -0300

    feat: add http module

--- src/infra/index.ts
+++ src/infra/index.ts
@@ -3,3 +3,4 @@ export * from './generation/generation.module';
 export * from './json-web-token/json-web-token.module';
 export * from './mail/mail.module';
 export * from './database/database.module';
+export * from './http/http.module';
