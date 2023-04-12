51e39b25841d18bfaed7db73d155187dc6a564ef
João Pedro <joaopedromoraesnsilva@gmail.com>
2023-04-11 21:41:53 -0300

    refactor: add validation pipe custom and logger custom

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ConsoleLogger } from '@nestjs/common';
+import {
+  ValidationPipeCustom,
+  LoggerCustom,
+  AbstractLogRepository,
+} from '@/custom';
+import { AppModule } from '@/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
+
+  app.useLogger(
+    new LoggerCustom(
+      new ConsoleLogger(),
+      app.get<AbstractLogRepository>(AbstractLogRepository),
+    ),
+  );
+
+  app.useGlobalPipes(new ValidationPipeCustom());
+
   await app.listen(3000);
 }
 bootstrap();
